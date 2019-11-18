6668b3e2ad8eb816bbd51eeb8a806815b17895a0
zyf19 <56283341+zyf19@users.noreply.github.com>
2019-11-18 17:47:05 +0800

    Add files via upload

---
+++ 11.18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+
+{
+    int a, b, n, i;
+    int sum = 0;
+    printf("Enter n");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (a = 1, b = 1; b <= i; b++)
+        {
+            a = b * a;
+        }
+        sum += a;
+    }
+    printf("%d", sum);
+    return 0;
+}
